9f378cdcb71c699fdef5686d13c251a8cf6e0ba0
Lowy Shin <lowyshin@littleworld.net>
2025-09-16 01:21:02 +0900

    Add comments to sqlnet_put.bat for documentation
    Added comments for clarity and usage instructions.

--- giipscripts/sqlnet_put.bat
+++ giipscripts/sqlnet_put.bat
@@ -1,1 +1,5 @@
+REM DPA - Database Performance Analysis
+REM sql server version
+REM mysql (on linux) : https://github.com/LowyShin/giipAgentLinux/blob/master/giipscripts/execmysql.sh
+
 powershell -ExecutionPolicy Bypass -File collect_sql_net_inventory.ps1
