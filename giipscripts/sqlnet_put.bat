54745056b7f38e4c47648fdc4dc21318d3bc7dd5
Lowy Shin <lowyshin@littleworld.net>
2025-09-16 01:22:01 +0900

    Update sqlnet_put.bat with contact info
    Added contact information for DPA assistance.

--- giipscripts/sqlnet_put.bat
+++ giipscripts/sqlnet_put.bat
@@ -2,4 +2,6 @@ REM DPA - Database Performance Analysis
 REM sql server version
 REM mysql (on linux) : https://github.com/LowyShin/giipAgentLinux/blob/master/giipscripts/execmysql.sh
 
+REM call me when you want use DPA, we can help to use. contact@littleworld.net
+
 powershell -ExecutionPolicy Bypass -File collect_sql_net_inventory.ps1
